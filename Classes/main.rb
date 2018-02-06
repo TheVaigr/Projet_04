03417548876a681d954dc7b4082cabde35ced86a
vDeaZ <MEzzUn74r,n>
2018-02-06 11:53:34 +0100

    10

--- Classes/main.rb
+++ Classes/main.rb
@@ -1,11 +1,5 @@
 require 'gosu'
 
-require_relative '../IHM/main_ihm'
+require_relative '../Controller/controller'
 
-WindowWidth = 1920
-WindowHeight = 1080
-Couleur = 1
-Pseudo = "bla"
-
-Window = MainIHM.new(WindowWidth, WindowHeight, Pseudo, Couleur)
-Window.show
+Controller = Controller.new()
--- Model/model.rb
+++ Model/model.rb
@@ -2,21 +2,21 @@ require 'gosu'
 
 require_relative '../Classes/hero'
 require_relative '../Classes/Ennemis/gardien'
+require_relative '../Classes/Armes/canon'
 
 class Model
 
-  attr_accessor :hero, :niveauDifficulte, :vitesseAutoScroll, :ennemis
+  attr_accessor :hero, :niveauDifficulte
 
   def initialize(niveauDifficulte, pseudo, couleur, width, height)
     @niveauDifficulte = niveauDifficulte
     @hero = Hero.new(pseudo, arme = Canon.new(), couleur, width/2, height-100)
-    @vitesseAutoScroll = @niveauDifficulte * 1.25
-    @ennemis = []
-    @ennemis[0] = Gardien.new(900,50)
-
   end
 
-  def Collision(position1, position2)
+"#{  def Collision(position1, position2)
     if position1.xa
+
     end
+  end}"
+
 end
